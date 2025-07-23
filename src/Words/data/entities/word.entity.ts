e8c53a045b53fdf1314e661168496ea0e3a9a04d
diegobejar2004 <diegobejar2004@gmail.com>
2025-07-22 20:48:32 -0600

    words: domain: interfaces: fixed, data: entities: fixed

--- src/Words/data/entities/word.entity.ts
+++ src/Words/data/entities/word.entity.ts
@@ -2,6 +2,8 @@ import { WordI } from "src/Words/domain/entitiesI/WordI";
 import { WordMeaningI } from "src/Words/domain/entitiesI/WordMeaningI";
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { WordMeaningEntity } from "./word_meaning.entity";
+import { WordOccupationI } from "src/Occupation/domain/entitiesI/WordOccupationI";
+import { WordOccupationEntity } from "src/Occupation/data/entities/word_occupation.entity";
 
 @Entity('palabra')
 export class WordEntity implements WordI {
@@ -11,4 +13,6 @@ export class WordEntity implements WordI {
     word: string;
     @OneToMany(() => WordMeaningEntity, wordMeaning => wordMeaning.word)
     meanings: WordMeaningI[];
+    @OneToMany(() => WordOccupationEntity, wordOccupation => wordOccupation.word)
+    occupations: WordOccupationI[];
 }
--- src/Words/domain/entitiesI/WordI.ts
+++ src/Words/domain/entitiesI/WordI.ts
@@ -1,7 +1,9 @@
+import { WordOccupationI } from "src/Occupation/domain/entitiesI/WordOccupationI";
 import { WordMeaningI } from "./WordMeaningI";
 
 export interface WordI {
     id: number;
     word: string;
     meanings: WordMeaningI[];
+    occupations: WordOccupationI[];
 }
